e4d56a6911e50928025744192e4e38181a2569dc
junha <goodjunha@gmail.com>
2023-12-13 16:04:34 +0900

    [add] :: #1 - SwitGrapherArrow

---
+++ Sources/SwitGrapPluginSupport/SwitGrapherArrow.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+/// 출력 그래프에서의 GraphViz "에지" (화살표)를 정의합니다.
+public struct SwitGrapherArrow: Hashable {
+
+    /// 에지가 시작하는 그래프 노드의 이름입니다.
+    public let origin: String
+
+    /// 에지가 가리키는 그래프 노드의 이름입니다.
+    public let destination: String
+
+    /// 에지의 색상입니다. 예: `#FF0000`
+    public let color: String
+
+    public init(origin: String, destination: String, color: String = "#000000") {
+        self.origin = origin
+        self.destination = destination
+        self.color = color
+    }
+
+}
